7990bbeaa4fb76ad762b501540b6f09b2efc863d
Abdullah-Samra <121175585+Abdullah-Samra@users.noreply.github.com>
2022-12-21 16:04:21 +0200

    Add files via upload

---
+++ Prime number/Prime number/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //عبدالله أحمد فتحي محمد محمد الحنفي سمره 
+
+            bool flag = false;
+            Console.Write("Enter first number: ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nThe Prime number from {0} to {1} is:", n1, n2);
+            Console.WriteLine("______________________________________\n");
+
+            for (; n1 <= n2; n1++)
+            {
+                for (int i = n1 - 1; 1 < i; i--)
+                {
+                    if (n1 % i != 0)
+                    {
+                        flag = true;
+                    }
+                    else
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (n1 == 2)
+                {
+                    flag = true;
+                }
+                if (flag == true)
+                    Console.WriteLine("\n" + n1);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
