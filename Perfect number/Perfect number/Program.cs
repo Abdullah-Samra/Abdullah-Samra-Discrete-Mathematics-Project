1f26318991183c01adab2a66c51be4f552b3ba5c
Abdullah-Samra <121175585+Abdullah-Samra@users.noreply.github.com>
2022-12-21 16:03:35 +0200

    Add files via upload

---
+++ Perfect number/Perfect number/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perfect_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //عبدالله أحمد فتحي محمد محمد الحنفي سمره 
+
+            Console.Write("Enter first number: ");
+            long n1 = long.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            long n2 = long.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nThe Perfect number from {0} to {1} is:", n1, n2);
+            Console.WriteLine("______________________________________\n");
+            for (; n1 <= n2; n1++)
+            {
+                long sum = 0;
+                for (long i = n1 - 1; i >= 1; i--)
+                {
+                    if (n1 % i == 0)
+                        sum = sum + i;
+                }
+
+                if (sum == n1)
+                    Console.WriteLine("\n" + n1);
+            }
+            Console.ReadKey();
+        }
+    }
+}
